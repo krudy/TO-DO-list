255ade45c9e2654bb5eda86a23489f904c1fbdef
TheLastSpoonbender <k.rudnicki1990@gmail.com>
2023-12-11 22:02:24 +0100

    add container element

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,7 @@
-console.log('asdasd');
+const tasksContainerElement = document.querySelector(".tasks");
+const tasks = ["laundry ", "walk the dog", "buy a newspaper"];
+const render = () => {
+    tasks.forEach((task) => {
+        const taskElement = document.createElement("li");
+    });
+};
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('asdasd');
+const tasksContainerElement: HTMLElement = document.querySelector(".tasks");
+
+const tasks: String[] = ["laundry ", "walk the dog", "buy a newspaper"];
+
+const render = () => {
+    tasks.forEach((task) => {
+        const taskElement: HTMLElement = document.createElement("li");
+    })
+};
